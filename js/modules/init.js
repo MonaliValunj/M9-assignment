89525641b660ec04839c529fa170ebc379c9ac1c
Monali Valunj <monavalunj96@gmail.com>
2023-12-06 14:36:53 -0800

    done

--- js/modules/init.js
+++ js/modules/init.js
@@ -1,13 +1,14 @@
-const xhr = new XMLHttpRequest()
-xhr.responseType = 'json'
-xhr.onreadystatechange = () => {
-    if(xhr.readyState == 4 && xhr.status == 200 ){
-        let employees = xhr.response
-        for (let employee of employees){
-            document.body.innerHTML = `${employee.name} <br>${employee.title} `
-        }
-    }
-}
-xhr.open('GET', '/data/employees.json')
-xhr.send()
-xhr.onerror = (e) => console.log(e.message)
+async function fetchEmployeeData() {
+    try {
+        const response = await fetch('./data/employees.json');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error loading employee data:', error);
+        throw error;
+      }
+  }
+
+  export { fetchEmployeeData };
+
+  
